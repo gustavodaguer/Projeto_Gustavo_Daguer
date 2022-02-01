09e5e38fb30d846855fba10f134146481ad1d63c
gustavocdaguer <gustavocdaguer@gmail.com>
2022-02-01 14:42:52 -0300

    Adicionado Método MB3()

--- ClasseB.cpp
+++ ClasseB.cpp
@@ -9,12 +9,17 @@ class B{
 		
 		void MB1();
 		void MB2();
+		void MB3();
 }
 
-void B::MB1(){]
+void B::MB1(){
 	cout << "Esse é o método MB1!" << endl;
 }
 
-void B::MB2(){]
+void B::MB2(){
 	cout << "Esse é o método MB2!" << endl;
+}
+
+void B::MB3(){
+	cout << "Esse é o método MB3!" << endl;
 }
